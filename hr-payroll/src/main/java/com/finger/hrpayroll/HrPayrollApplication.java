f5c412e5cc6f1898ba7ec4a56a5dc62a63fbc32d
Vinícius Finger <fingervinicius@icloud.com>
2021-01-11 15:02:35 -0300

    Balanceamento de carga com Ribbon

--- hr-payroll/src/main/java/com/finger/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/finger/hrpayroll/HrPayrollApplication.java
@@ -2,9 +2,11 @@ package com.finger.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
+@RibbonClient(name = "hr-worker")
 @SpringBootApplication
 public class HrPayrollApplication {
 
--- hr-worker/src/main/java/com/finger/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/finger/hrworker/resources/WorkerResource.java
@@ -2,7 +2,10 @@ package com.finger.hrworker.resources;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +19,11 @@ import com.finger.hrworker.repositories.WorkerRepository;
 @RequestMapping("/workers")
 public class WorkerResource {
 	
+	private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+	
+	@Autowired
+	private Environment env;
+	
 	@Autowired
 	private WorkerRepository workerRepository;
 	
@@ -27,6 +35,8 @@ public class WorkerResource {
 	
 	@GetMapping("/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id){
+		logger.info("port = " + env.getProperty("local.server.port"));
+		
 		Worker worker = workerRepository.findById(id).orElse(null);
 		return ResponseEntity.ok().body(worker);
 	}
