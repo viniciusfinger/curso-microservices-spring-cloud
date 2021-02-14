efa7375cff62ef233e0f83138856305c643124b1
Vinícius Finger <fingervinicius@icloud.com>
2021-02-14 14:28:26 -0300

    Criacao do projeto hr-worker

---
+++ hr-worker/src/main/java/com/finger/hrworker/HrWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.finger.hrworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrWorkerApplication.class, args);
+	}
+
+}
