3e03b921a8efa2248a31c9a6a7a27b76a9e5b4af
Vinícius Finger <fingervinicius@icloud.com>
2021-02-14 14:17:19 -0300

    Reset do repo para começar o curso do zero novamente

--- hr-worker/src/main/java/com/finger/hrworker/repositories/WorkerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.finger.hrworker.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.finger.hrworker.entities.Worker;
-
-public interface WorkerRepository extends JpaRepository<Worker, Long> {}
