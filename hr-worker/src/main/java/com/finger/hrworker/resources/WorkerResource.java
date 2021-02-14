6aad27e61f3cf026fd7f2fe83f61faeab875884d
Vinícius Finger <fingervinicius@icloud.com>
2021-02-14 15:02:03 -0300

    endpoints worker

--- hr-worker/src/main/java/com/finger/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/finger/hrworker/resources/WorkerResource.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,9 +21,16 @@ public class WorkerResource {
 	@Autowired
 	private WorkerRepository repository;
 	
+	@GetMapping
 	public ResponseEntity<List<Worker>> findAll() {
 		List<Worker> list = repository.findAll();
 		return ResponseEntity.ok(list);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Worker> findById(@PathVariable long id) {
+		Worker worker = repository.findById(id).orElse(null);
+		return ResponseEntity.ok(worker);
 	}
 	
 }
