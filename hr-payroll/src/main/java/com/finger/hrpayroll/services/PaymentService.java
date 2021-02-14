218a14b1f660e388d1f4402ab94067647dd50f2f
Vinícius Finger <fingervinicius@icloud.com>
2021-02-14 15:32:02 -0300

    Integracao entre os dois servicos com restTemplate

--- hr-payroll/src/main/java/com/finger/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/finger/hrpayroll/services/PaymentService.java
@@ -1,15 +1,34 @@
 package com.finger.hrpayroll.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.finger.hrpayroll.entities.Payment;
+import com.finger.hrpayroll.entities.Worker;
 
 @Service
 public class PaymentService {
+	
+	@Value("${hr-worker.host}")
+	private String workerHost;
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
 	public Payment getPayment(long workerId, int days) {
+		Map<String, String> uriVariables = new HashMap();
+		uriVariables.put("id", ""+workerId);
+		
+		Worker worker = restTemplate.getForObject(workerHost + "workers/{id}", Worker.class, uriVariables);
+		
 		Payment payment = new Payment();
-		payment.setName("Bob");
-		payment.setDailyIncome(200.0);
+		payment.setName(worker.getName());
+		payment.setDailyIncome(worker.getDailyIncome());
 		payment.setDays(days);
 		return payment;
 	}
