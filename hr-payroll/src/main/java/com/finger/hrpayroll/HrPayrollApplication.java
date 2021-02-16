a140dbf1c0fd2093cb224bd643009a5f10bbc38b
Vinícius Finger <fingervinicius@icloud.com>
2021-02-16 11:09:55 -0300

    Implementando Feign Clients

--- hr-payroll/src/main/java/com/finger/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/finger/hrpayroll/HrPayrollApplication.java
@@ -2,12 +2,13 @@ package com.finger.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class HrPayrollApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrPayrollApplication.class, args);
 	}
-
 }
--- hr-payroll/src/main/java/com/finger/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/finger/hrpayroll/services/PaymentService.java
@@ -1,30 +1,22 @@
 package com.finger.hrpayroll.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.finger.hrpayroll.entities.Payment;
 import com.finger.hrpayroll.entities.Worker;
+import com.finger.hrpayroll.feignClients.WorkerFeignClient;
+
 
 @Service
 public class PaymentService {
 	
-	@Value("${hr-worker.host}")
-	private String workerHost;
-	
 	@Autowired
-	private RestTemplate restTemplate;
+	private WorkerFeignClient workerFeignClient;
 	
 	public Payment getPayment(long workerId, int days) {
-		Map<String, String> uriVariables = new HashMap();
-		uriVariables.put("id", ""+workerId);
 		
-		Worker worker = restTemplate.getForObject(workerHost + "workers/{id}", Worker.class, uriVariables);
+		Worker worker = workerFeignClient.findById(workerId).getBody(); 
 		
 		Payment payment = new Payment();
 		payment.setName(worker.getName());
