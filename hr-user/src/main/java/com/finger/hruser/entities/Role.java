c17b83371584f84b1a268ed1ca5e8464380921f9
Vinícius Finger <fingervinicius@icloud.com>
2021-02-17 10:27:47 -0300

    Mapeamento de role e user no banco de dados

---
+++ hr-user/src/main/java/com/finger/hruser/entities/Role.java
@@ -0,0 +1,36 @@
+package com.finger.hruser.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_role")
+public class Role {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String roleName;
+	
+	public Role() {}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getRoleName() {
+		return roleName;
+	}
+
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
+}
