a1ed9405ffe695940a836b1f8afe78293729bffc
Vinícius Finger <fingervinicius@icloud.com>
2021-02-17 11:15:03 -0300

    criacao projeto oauth

---
+++ hr-oauth/src/main/java/com/finger/hroauth/HrOauthApplication.java
@@ -0,0 +1,15 @@
+package com.finger.hroauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class HrOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrOauthApplication.class, args);
+	}
+
+}
